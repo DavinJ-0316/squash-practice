32fef2c17d5cee6586405e9a408b2b404d2bda63
DavinJ-0316 <jinhaoj@utas.edu.au>
2022-09-21 13:28:24 +1000

    7th

--- squash.js
+++ squash.js
@@ -1,4 +1,9 @@
 const array1 = [{id: {uuid: 1}}, {id: {uuid: 2}}, {id: {uuid: 3}}, {id: {uuid: 4}}, {id: {uuid: 5}}, {id: {uuid: 6}}, {id: {uuid: 7}}];
+const test1 = 1;
+const test2 = 2;
+const test3 = 3;
+const test4 = 4;
+const test5 = 5;
 
 // 0 + 1 + 2 + 3 + 4
 const initialValue = 0;
